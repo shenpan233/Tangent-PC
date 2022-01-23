fe31b2b96f2b40eee60b1e91f0e82f551be5fc96
shenpan233 <shenpan233@vip.qq.com>
2022-01-23 22:03:12 +0800

    更新一下撤回群消息

---
+++ PCQQ/Fun3F7.go
@@ -0,0 +1,14 @@
+/*
+  @Author:  Trial(Trialpro@gmail.com)
+  @Creat:   2022/1/23 22:01
+  @Notice:
+*/
+
+package PCQQ
+
+import "Tangent-PC/protocal/Protobuf/im/cs/cmd0x3f7"
+
+func (this *TangentPC) pack0x3f7(GroupCode uint64, MsgSeq uint32) []byte {
+	buffer := cmd0x3f7.GetBuffer(GroupCode, MsgSeq)
+	return buffer
+}
